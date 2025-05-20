1103f6d1709f5f87f183d42e12e2cb6afed6da57
ssalgadoi <se.salgado@duocuc.cl>
2025-05-20 01:27:48 -0400

    Actualizar un evento

--- controllers/events.js
+++ controllers/events.js
@@ -17,7 +17,7 @@ const crearEvento = async (req, res = response) => {
     const evento = new Evento(req.body);
 
     try {
-        evento.user = req.uid; 
+        evento.user = req.uid;
         const eventoGuardado = await evento.save();
 
         res.json({
@@ -36,12 +36,47 @@ const crearEvento = async (req, res = response) => {
 };
 
 
-const actualizarEvento = (req, res = response) => {
+const actualizarEvento = async (req, res = response) => {
+    const eventoId = req.params.id
+    const uid = req.uid;
 
-    res.json({
-        ok: true,
-        msg: 'actualiarEvento'
-    });
+    try {
+        const evento = await Evento.findById(eventoId);
+
+        if (!evento) {
+            res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            })
+        }
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento'
+            });
+        }
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true });
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+
+    }
+
+
 }
 
 const eliminarEvento = (req, res = response) => {
