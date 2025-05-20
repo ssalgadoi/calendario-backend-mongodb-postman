b61260905cb0cb4eb8b198efb7ad05a52d3de451
ssalgadoi <se.salgado@duocuc.cl>
2025-05-20 01:39:59 -0400

    Eliminamos evento

--- controllers/events.js
+++ controllers/events.js
@@ -44,7 +44,7 @@ const actualizarEvento = async (req, res = response) => {
         const evento = await Evento.findById(eventoId);
 
         if (!evento) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existe por ese id'
             })
@@ -79,14 +79,39 @@ const actualizarEvento = async (req, res = response) => {
 
 }
 
-const eliminarEvento = (req, res = response) => {
+const eliminarEvento = async (req, res = response) => {
+    const eventoId = req.params.id;
+    const uid = req.uid; // ID del usuario autenticado (desde el middleware validarJWT)
 
-    res.json({
-        ok: true,
-        msg: 'eliminarEvento'
-    });
-}
+    try {
+        const evento = await Evento.findById(eventoId);
 
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no encontrado por ese ID'
+            });
+        }
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de eliminar este evento'
+            });
+        }
+
+        await Evento.findByIdAndDelete(eventoId);
+
+        res.json({ok: true});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+};
 
 
 module.exports = {
